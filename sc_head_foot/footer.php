b4026fcbead9afd52e45961f4233d0eb46e20120
SSS241999 <shubhamshikari241999@gmail.com>
2021-03-01 04:41:51 +0530

    First commit

---
+++ sc_head_foot/footer.php
@@ -0,0 +1,59 @@
+<DOCTYPE html>
+    <html>
+<head> <link rel = "stylesheet" type ="text/css" href="footer.css"> </head>
+<footer class = "text-light">
+<div class="container">
+            <div class="row">
+                <div class="col-md-3 col-lg-4 col-xl-3">
+                    <br>
+                    <h5>About</h5>
+                    <hr class="bg-white mb-2 mt-0 d-inline-block mx-auto w-25">
+                    <p class="mb-0">
+                        4tAble is a website made to combine the rich and poor digitally and eradicate the poverty of India.
+                    </p>
+                </div>
+
+                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto">
+                    <br>
+                    <h5>Information</h5>
+                    <hr class="bg-white mb-2 mt-0 d-inline-block mx-auto w-25">
+                    <ul class="list-unstyled">
+                        <li><a href="#" class="text-light bg-dark">shubhamshikari241999@gmail.com</a></li>
+
+                    </ul>
+                </div>
+
+                <div class="col-md-3 col-lg-2 col-xl-2 mx-auto">
+                    <br>
+                    <h5>Others links</h5>
+                    <hr class="bg-white mb-2 mt-0 d-inline-block mx-auto w-25">
+                    <ul class="list-unstyled">
+                        <li><a href="https://www.bbc.com/news/world-asia-india-28189645" class="text-light bg-dark">
+                                BBC India</a></li>
+                        <li><a href="https://www.riddhisiddhicharitabletrust.org/FCRA_details.html" class="text-light bg-dark">FCRA</a></li>
+                        <li><a href="https://timesofindia.indiatimes.com/topic/charity" class="text-light bg-dark">Times Of India</a></li>
+                        <li><a href="https://www.ndtv.com/topic/charity-donation" class="text-light bg-dark">NDTV</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-md-4 col-lg-3 col-xl-3">
+                    <br>
+                    <h5>Contact</h5>
+                    <hr class="bg-white mb-2 mt-0 d-inline-block mx-auto w-25">
+                    <ul class="list-unstyled">
+                        <li><i class="fa fa-home mr-2"></i> 4tAble</li>
+                        <li><i class="fa fa-envelope mr-2"></i> info@4table.com</li>
+                        <li><i class="fa fa-phone mr-2"></i> + 91 882-809-1851</li>
+                        <li><i class="fa fa-print mr-2"></i> + 91 750-602-5018</li>
+                    </ul>
+                </div>
+                <div class="col-12 copyright mt-3">
+                    <p class="float-left">
+                        <a href="#">Back to top</a>
+                    </p>
+                    <p class="text-right text-muted">4tAble <i class="fa fa-heart"></i>
+                </div>
+            </div>
+        </div>
+</footer>
+        </html>
